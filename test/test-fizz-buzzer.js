2ae48874da331828d09680af662f8948ed540850
Jorge R <jorgerodriguez434@gmail.com>
2018-03-05 10:31:05 -0600

    test fizzBuzzer

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -1,3 +1,39 @@
 const expect = require('chai').expect;
-const fizzBuzzer = require('./fizzBuzzer');
 
+const fizzBuzzer = require('../fizzBuzzer');
+
+describe('fizzBuzzer', () => {
+
+  it('should be divisible by 15', () => {
+    [15, 30, 45].map(num => {
+      expect(fizzBuzzer(num)).to.equal('fizz-buzz');
+    });
+  });
+
+  it('should be divisible by 3', () => {
+    [3, 6, 9, 12].map(num => {
+      expect(fizzBuzzer(num)).to.equal('fizz');
+    });
+  });
+
+  it('should be divisible by 5', () => {
+    [5, 10, 20].map(num => {
+      expect(fizzBuzzer(num)).to.equal('buzz');
+    });
+  });
+
+  it('should return number if not divisible by 3 or 5', () => {
+    [1, 2, 4, 7].map(num => {
+      expect(fizzBuzzer(num)).to.equal(num);
+    });
+  });
+
+  it('should produce error if input isn\'t number', () => {
+    const badInputs = [true, false, 'cat', ()=>{}, [1, 2, 3]];
+    badInputs.forEach(num => {
+      expect( () => {
+          fizzBuzzer(num);
+      }).to.throw(Error);
+    });
+  });
+});
